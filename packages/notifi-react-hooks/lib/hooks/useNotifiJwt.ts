1bb119fdbf79d5a91b4137815f687e71d4c63d6d
Juni Kim <73hKrn@gmail.com>
2022-02-14 23:07:17 -0800

    Use createLocalStorageStateHook
    This is a warning

--- packages/notifi-react-hooks/lib/hooks/useNotifiJwt.ts
+++ packages/notifi-react-hooks/lib/hooks/useNotifiJwt.ts
@@ -1,15 +1,17 @@
-import useLocalStorageState from "use-local-storage-state";
+import { createLocalStorageStateHook } from "use-local-storage-state";
 
 const LOCAL_STORAGE_KEY = "notifi:jwt";
+
+const useLocalStorage = createLocalStorageStateHook<string | null>(
+  LOCAL_STORAGE_KEY,
+  null
+);
 
 const useNotifiJwt = (): Readonly<{
   jwt: string | null;
   setJwt: (jwt: string | null) => void;
 }> => {
-  const [jwt, setJwt] = useLocalStorageState<string | null>(
-    LOCAL_STORAGE_KEY,
-    null
-  );
+  const [jwt, setJwt] = useLocalStorage();
   return { jwt, setJwt };
 };
 
