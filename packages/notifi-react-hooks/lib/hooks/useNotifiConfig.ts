20a293a9aca745805d8d4dfc6e250578fbefb1f5
nimesh-irisio <92230180+nimesh-irisio@users.noreply.github.com>
2022-02-22 21:33:26 -0800

    Adding env config (#3)
    * Adding env config
    
    * Update use-local-storage-state
    
    Don't know how it pickec v13 to start with, but that stuff is old and
    uses a weird incompatible version of react.
    
    Also move React and React-DOM to peerDependencies to allow client libs
    to potentially ask us to increase support for other versions.
    
    * Update lockfile
    
    This needs re-generation after blowing away node_modules
    
    * Updating README.md
    
    * Updating hooks README.md
    
    Co-authored-by: Nimesh Amin <nimesh.amin@notifi.network>
    Co-authored-by: Juni Kim <73hKrn@gmail.com>

--- packages/notifi-react-hooks/lib/hooks/useNotifiConfig.ts
+++ packages/notifi-react-hooks/lib/hooks/useNotifiConfig.ts
@@ -1,7 +1,32 @@
-const useNotifiConfig = () => {
+export enum BlockchainEnvironment {
+  MainNetBeta,
+  TestNet,
+  DevNet
+}
+
+const useNotifiConfig = (env = BlockchainEnvironment.MainNetBeta) => {
+  let gqlUrl = '';
+  switch (env) {
+    case BlockchainEnvironment.MainNetBeta:
+      gqlUrl = 'https://api.notifi.network/gql';
+      break;
+    case BlockchainEnvironment.TestNet:
+      gqlUrl = 'https://stg-api.notifi.network/gql';
+      break;
+    case BlockchainEnvironment.DevNet:
+      gqlUrl = 'https://dev-api.notifi.network/gql';
+      break;
+    default:
+      assertUnreachable(env);
+  }
+
   return {
-    gqlUrl: 'https://api.notifi.network/api/gql'
+    gqlUrl
   };
+};
+
+const assertUnreachable = (_x: never): never => {
+  throw new Error('This should never be reached');
 };
 
 export default useNotifiConfig;
